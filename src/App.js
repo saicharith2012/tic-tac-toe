db013621569338d52af12192e9ec3a5b1659a1cf
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-08-29 03:09:59 +0530

    each button component has its own state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
   return (
     // The Component can return only one tag.
     <div>
       <div className="first line">
         <h1>Welcome to my app.</h1>
-        <MyButton count={count} onClick={handleClick} />
+        <MyButton />
       </div>
       <div>
         <h2>Even Dead I'm the Hero.</h2>
-        <MyButton count={count} onClick={handleClick} />
+        <MyButton />
       </div>
     </div>
   );
 }
 
-function MyButton({ count, onClick }) {
-  return <button onClick={onClick}>Clicked {count} times</button>;
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return <button onClick={handleClick}>Clicked {count} times</button>;
 }
 export default App;
